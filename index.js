3c5f63718a647bb8ec07b90f60d17306f90533e5
maaskantje8394-svg <maaskantje8394@gmail.com>
2026-07-04 00:41:46 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import 'dotenv/config';
+import http from 'http';
 import {
     Client,
     GatewayIntentBits,
@@ -16,23 +17,36 @@ const client = new Client({
 const WELCOME_CHANNEL = '1520535431827951656';
 
 client.once(Events.ClientReady, () => {
-    console.log(`${client.user.tag} is online.`);
+    console.log(`✅ Logged in as ${client.user.tag}`);
 });
 
 client.on(Events.GuildMemberAdd, async (member) => {
     const channel = member.guild.channels.cache.get(WELCOME_CHANNEL);
     if (!channel) return;
 
-const embed = new EmbedBuilder()
-    .setColor('#000000')
-    .setDescription(`**Welcome to ${member.guild.name}** ${member}! 🌙`)
-    .setImage('https://cdn.discordapp.com/attachments/1518352163603091577/1522728390652723300/Bannder.jpg?ex=6a4986d3&is=6a483553&hm=e8ece5dd2a9b50cfe9b9af1d47655b7031a716a3ab80af0fac65a4926576aa50')
-    .setTimestamp();
+    const embed = new EmbedBuilder()
+        .setColor('#000000')
+        .setDescription(
+            `<:Clarity:1522719037610790923> Welcome ${member} to **${member.guild.name}**!`
+        )
+        .setImage(
+            'https://cdn.discordapp.com/attachments/1518352163603091577/1522728390652723300/Bannder.jpg?ex=6a4986d3&is=6a483553&hm=e8ece5dd2a9b50cfe9b9af1d47655b7031a716a3ab80af0fac65a4926576aa50'
+        )
+        .setTimestamp();
 
     await channel.send({
-        content: `${member}`,
         embeds: [embed]
     });
+});
+
+// Render / UptimeRobot
+const PORT = process.env.PORT || 3000;
+
+http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Clarity Bot is online!');
+}).listen(PORT, () => {
+    console.log(`🌐 Web server listening on port ${PORT}`);
 });
 
 client.login(process.env.TOKEN);
